43031dec44ba63ac3e4d60bde39efc1caa4c415e
Hsu Hsu Htet <hsuhsuhtet@Hsus-MacBook-Air.local>
2023-12-28 13:48:58 +0630

    useContext Hook

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import Child1 from "./components/Child1";
+import Child2 from "./components/Child2";
+import Child3 from "./components/Child3";
+import FirstContextProvider from "./components/firstContent";
+
+
+
+function App() {
+  
+  return (
+    <section>
+      <FirstContextProvider>
+      <Child1 />
+      <Child2 />
+      <Child3 />
+      
+      </FirstContextProvider>
+ 
+    </section>
+  );
+}
+
+export default App;
